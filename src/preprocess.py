8a4a2accfc8f67f46bb7c1eeb53a6a33a9e052df
Satej Gandre <ihs.satej@outlook.com>
2025-07-07 10:05:17 -0400

    initial commit

---
+++ src/preprocess.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from typing import Tuple
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
